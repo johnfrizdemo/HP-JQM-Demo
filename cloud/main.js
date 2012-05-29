bb880b2d27b140cc4d682931bec0690a98874fc9
johnfriz+demo@gmail.com <johnfriz+demo@gmail.com>
2012-05-29 17:38:56 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -44,31 +44,32 @@ function cachePoints(hash, data) {
 
 function getPoints(params,callback) {
   var response = {};
-  var cache    = getCachedPoints();
-
-  if (cache.length === 0) {
-    var data = MARKERS;
-	var crypto=require("crypto");
-	var md5=crypto.reateHash("md5");
-	var hash=md5.update(JSON.stringify(data)).digest();
-    // Cache the data
-    cachePoints(hash, data);
-
-    // Build the response
-    response = {'data': data, 'hash':hash, 'cached':false};
-  } else {
-    // Parse the cached data
-    cache = JSON.parse(cache);
-
-    if( params.hash && params.hash === cache.hash ) {
-      // Client data is up to date
-      response = {'hash':params.hash, 'cached':true};
-    } else {
-      // Hash value from client missing or incorrect, return cached cloud data
-      response = cache;
+  getCachedPoints({}, function(err, res) {
+    if( res && res.value && res.value.length > 0 ) {
+      // Parse the cached data
+      var cache = JSON.parse(res.value);
+  
+      if( params.hash && params.hash === cache.hash ) {
+        // Client data is up to date
+        response = {'hash':params.hash, 'cached':true};
+      } else {
+        // Hash value from client missing or incorrect, return cached cloud data
+        response = cache;
+      }      
     }
-  }
-  callback(null, response);
+    else {
+      var data = MARKERS;
+      var crypto=require("crypto");
+      var md5=crypto.reateHash("md5");
+      var hash=md5.update(JSON.stringify(data)).digest();
+      // Cache the data
+      cachePoints(hash, data);
+  
+      // Build the response
+      response = {'data': data, 'hash':hash, 'cached':false};
+    } 
+    callback(null, response);
+  });
 }
 
 module.exports={

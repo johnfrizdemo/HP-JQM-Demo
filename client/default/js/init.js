2830763ca1d6d8e1c352ca6b56f3055c9933098e
johnfriz+demo@gmail.com <johnfriz+demo@gmail.com>
2012-05-30 11:39:32 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -2,9 +2,6 @@
 $(document).ready(function() {  
 	importViews(function() {
     bindEvent();
-    setTimeout(function() {
-      changeView("home");
-    }, 10000);
-		
+    changeView("home");    
 	});
 });
